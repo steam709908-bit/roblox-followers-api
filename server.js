47eeec78e6b157098b717cc6beba324c8eea11dd
Kevin <steam709908@gmail.com>
2026-02-17 14:55:31 -0600

    primer commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/followers/:userid", async (req, res) => {
+    try {
+        const userId = req.params.userid;
+
+        const response = await axios.get(
+            `https://friends.roblox.com/v1/users/${userId}/followers/count`
+        );
+
+        res.json({
+            success: true,
+            followers: response.data.count
+        });
+
+    } catch (error) {
+        res.json({
+            success: false,
+            error: "No se pudo obtener seguidores"
+        });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Servidor corriendo en puerto " + PORT);
+});
